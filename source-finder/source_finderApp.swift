e966215012575a7c2937767ab74d9dba756b11a0
Olaf Neumann <github@olafneumann.org>
2023-04-17 11:01:17 +0200

    Initial Commit

---
+++ source-finder/source_finderApp.swift
@@ -0,0 +1,17 @@
+//
+//  source_finderApp.swift
+//  source-finder
+//
+//  Created by Olaf Neumann on 17.04.23.
+//
+
+import SwiftUI
+
+@main
+struct source_finderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
